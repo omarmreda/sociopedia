61e6b8739a433fab9ddfcce9d3e697929a2d92ae
omarmreda95 <omarmreda95@gmail.com>
2023-09-19 13:33:46 +0200

    adding images

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,3 +1,39 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import User from '../models/User.js';
+
+/* REGISTER USER */
+export const register = async(req, res) => {
+    try{
+        const {
+            firstName,
+            lastName,
+            email,
+            password,
+            picturePath,
+            friends,
+            location,
+            occupation
+        } = req.body;
+
+        const salt = await bcrypt.genSalt();
+        const passwordHash = await bcrypt.hash(password, salt);
+
+        const newUSer = new User({
+            firstName,
+            lastName,
+            email,
+            password: passwordHash,
+            picturePath,
+            friends,
+            location,
+            occupation,
+            viewedProfile: Math.floor(Math.random() * 10000),
+            impressions: Math.floor(Math.random() * 10000)
+        })
+        const savedUser = await newUSer.save();
+        res.status(201).json(savedUser);
+    }catch (err){
+        res.status(500).json({ error: err.message })
+    }
+}
